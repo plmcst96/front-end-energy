5005da9c15d920271402356c535da642d85b0c59
NoemiP94 <m_emi94@hotmail.it>
2024-01-31 12:53:20 +0100

    add fetch to province component

--- src/components/province/Province.jsx
+++ src/components/province/Province.jsx
@@ -1,7 +1,36 @@
 import { Container, Row, Col, Button, Pagination } from 'react-bootstrap'
 import ProvinceElement from './ProvinceElement'
+import { useState, useEffect } from 'react'
 
 const Province = () => {
+  const [province, setProvince] = useState([])
+  const getProvince = () => {
+    fetch('http://localhost:3001/provinces?page=0&size=10&orderBy=name', {
+      headers: {
+        Authorization:
+          'Bearer eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiIyYzJmZjY2Ni02YTlmLTQxMDItOGNlYy00MGUyNTVhMTcyMWUiLCJpYXQiOjE3MDY3MDA1OTcsImV4cCI6MTcwNzMwNTM5N30.SLeqiiCVTUMQZdHnkcdMgLAv1u3Y6LxtAqRO3r9nOeLgfky3IvBKvGujaJ4dFV_3 ',
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error('Errore nel recupero delle province')
+        }
+      })
+      .then((data) => {
+        console.log('Fetch completata, dati recuperati', data)
+        setProvince(data)
+      })
+      .catch((error) => {
+        console.log('Error!', error)
+      })
+  }
+
+  useEffect(() => {
+    getProvince()
+  }, [])
+
   return (
     <Container>
       <Row className="flex-column">
