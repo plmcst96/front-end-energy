81e6cf646bbe61a36e80b692ddaa9b4b5993236a
Cristina Palmisani <cristina.palmisani@outlook.it>
2024-01-31 16:15:34 +0100

    correzione store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,5 +1,5 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import registerReducer from "../reducers/register/login";
+import registerReducer from "../reducers/register";
 
 const bigReducer = combineReducers({
   register: registerReducer,
