51b647894ee140a9c9569f8301fbc3ca2153335a
Cristina Palmisani <cristina.palmisani@outlook.it>
2024-01-31 19:44:40 +0100

    correzzione navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { Container, Image, Nav, NavDropdown, Navbar } from "react-bootstrap"
+import { Container, Image, Nav, Navbar } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import logo from "../logo.png"
 
@@ -8,7 +8,7 @@ const NavBar = () => {
       <Container>
         <Image
           src={logo}
-          className="rounded-circle"
+          className="rounded-circle me-5"
           style={{ width: "70px" }}
         />
 
@@ -16,29 +16,28 @@ const NavBar = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link>
-              <Link to="/">Home</Link>
+              <Link to="/" className="text-decoration-none text-black">
+                Home
+              </Link>
             </Nav.Link>
-            <Nav.Link>Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item>Action</NavDropdown.Item>
-              <NavDropdown.Item>Another action</NavDropdown.Item>
-              <NavDropdown.Item>Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item>Separated link</NavDropdown.Item>
-            </NavDropdown>
             <Nav.Link>
               <Link to="/clients" className="text-decoration-none text-black">
                 Clients
               </Link>
             </Nav.Link>
+
             <Nav.Link>
-              <Link to="/login" className="text-decoration-none text-black">
-                Login/SingUP
+              <Link to="/address" className="text-decoration-none text-black">
+                Invoice
               </Link>
             </Nav.Link>
             <Nav.Link>
-              <Link to="/address" className="text-decoration-none text-black">
-                Address
+              <Link
+                to="/login"
+                className="text-decoration-none text-black"
+                style={{ marginLeft: "800px" }}
+              >
+                Login/SingUP
               </Link>
             </Nav.Link>
           </Nav>
