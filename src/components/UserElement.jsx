34b9192c79f8d58c4e6987f18c756474b5653fd8
NoemiP94 <m_emi94@hotmail.it>
2024-02-01 17:23:57 +0100

    add elemnts to userElement component fot getUser

--- src/components/UserElement.jsx
+++ src/components/UserElement.jsx
@@ -1,10 +1,19 @@
+import { useEffect } from 'react'
 import { Col, Container, ListGroup, Row } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { getUsers } from '../redux/action/users'
 
 const UserElement = () => {
+  const userData = useSelector((state) => state.user.content)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getUsers())
+  }, [dispatch])
+
   return (
     <Container fluid>
       <Row className="flex-column">
-        <Col>
+        <Col className="d-flex justify-content-around mt-5">
           <div className="me-5 ps-5">
             <h3>Id</h3>
           </div>
@@ -21,12 +30,45 @@ const UserElement = () => {
             <h3>Email</h3>
           </div>
           <div className="me-5 ps-5">
+            <h3>Avatar</h3>
+          </div>
+          <div className="me-5 ps-5">
             <h3>Role</h3>
           </div>
         </Col>
         <Col>
-          <ListGroup>
-            <ListGroup.Item></ListGroup.Item>
+          <ListGroup className="my-3">
+            {userData &&
+              userData.map((user, index) => {
+                return (
+                  <ListGroup.Item
+                    key={index}
+                    className="d-flex justify-content-evenly"
+                  >
+                    <Col sm={1} className="me-5">
+                      {user.uuid}
+                    </Col>
+                    <Col sm={1} className="me-5 ps-3">
+                      {user.name}
+                    </Col>
+                    <Col sm={1} className="me-5 ps-3">
+                      {user.surname}
+                    </Col>
+                    <Col sm={1} className="me-5 ps-3">
+                      {user.username}
+                    </Col>
+                    <Col sm={1} className="me-5 ps-3">
+                      {user.email}
+                    </Col>
+                    <Col sm={1} className="me-5 ps-3">
+                      {user.avatar}
+                    </Col>
+                    <Col sm={1} className="me-5 ps-5">
+                      {user.role}
+                    </Col>
+                  </ListGroup.Item>
+                )
+              })}
           </ListGroup>
         </Col>
       </Row>
