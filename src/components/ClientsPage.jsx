3f47b3b4c4abe515c2e536ded56eb9d976950778
guimasi1 <143355576+guimasi1@users.noreply.github.com>
2024-02-02 15:52:12 +0100

    fixes admin/user in clientsPage

--- src/components/ClientsPage.jsx
+++ src/components/ClientsPage.jsx
@@ -12,6 +12,7 @@ import { getAddress } from "../redux/action";
 const ClientsPage = () => {
   const dispatch = useDispatch();
   const tokenStorage = localStorage.getItem("token");
+  const role = localStorage.getItem("role");
   const addressData = useSelector((state) => state.address.list);
 
   const [show, setShow] = useState(false);
@@ -34,9 +35,6 @@ const ClientsPage = () => {
     dispatch(getAllCLients(filtersClients, tokenStorage));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [filtersClients]);
-
-  const role = useSelector((state) => state.register.role);
-  const token = useSelector((state) => state.register.token);
 
   useEffect(() => {
     dispatch(getAddress(filtersClients, tokenStorage));
@@ -278,8 +276,8 @@ const ClientsPage = () => {
             variant="primary"
             onClick={() => {
               handleClose();
-              dispatch(addClient(newClient, token)).then(() => {
-                dispatch(getAllCLients(filtersClients, token));
+              dispatch(addClient(newClient, tokenStorage)).then(() => {
+                dispatch(getAllCLients(filtersClients, tokenStorage));
               });
             }}
           >
@@ -315,7 +313,9 @@ const ClientsPage = () => {
                 >
                   Add client
                 </Button>
-              ) : null}
+              ) : (
+                ""
+              )}
             </div>
           </div>
         </Col>
